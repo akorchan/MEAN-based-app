a7b54c6106893c8101094f75adefc3d7c6214521
Andrey Korchan <gprogramador@gmail.com>
2014-10-07 12:50:37 +0300

    work with sections

--- public/scripts/controllers/words-controller.js
+++ public/scripts/controllers/words-controller.js
@@ -1,13 +1,17 @@
 'use strict';
 
 /** Controllers */
-angular.module('app.controllers').controller('WordsController', function ($scope, $routeParams, $location, WordsService) {
-
+angular.module('app.controllers').controller('WordsController', function ($scope, $routeParams, $document, WordsService) {
     $scope.word = $routeParams.word
     if (typeof $scope.word !== 'undefined') {
         WordsService.getMeaning($scope.word, 'ru', function (data) {
             $scope.meanings = data;
+            $document.scrollToElementAnimated(angular.element(document.getElementById('section-2')));
         });
+    }
+
+    $scope.toSection2 = function() {
+        $document.scrollToElementAnimated(angular.element(document.getElementById('section-2')));
     }
 
 });
