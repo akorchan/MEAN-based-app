00e22184472ed4175d855f6f7c3bb1357142f5ce
Andrey Korchan <gprogramador@gmail.com>
2014-10-07 11:54:14 +0300

    refactor controllers

--- public/scripts/controllers/main-controller.js
+++ public/scripts/controllers/main-controller.js
@@ -1,37 +1,7 @@
 'use strict';
 
 /** Controllers */
-angular.module('app.controllers').controller('MainController', function ($scope, $document, WordsService) {
-    $scope.selected = undefined;
-    $scope.words = [];
-//    var isAutocompelteEnabled = true;
-    $scope.onedit = function () {
-//        if (isAutocompelteEnabled) {
-        $scope.words = [];
-        WordsService.searchWords($scope.selected, 'ru', function (searchingData) {
-//                isAutocompelteEnabled = false;
-            $scope.words = searchingData;
-//            duplicates apears here
-            /*                console.log(searchingData + ' : ' + searchingData);
-             if (angular.isArray(searchingData)) {
-             $scope.words = [];
-             searchingData.reduce(function (sequence, word) {
-             return sequence.then(function () {
-             return WordsService.getMeaningSync(word, 'ru')
-             }).then(function (meaning) {
-             if (meaning.length > 0) {
-             $scope.words.push(word);
-             } else {
-             console.log(word + "-" + meaning);
-             }
-             });
-             }, Promise.resolve()).then(function () {
-             isAutocompelteEnabled = true;
-             });
-             }*/
-        });
-//        }
-    }
+angular.module('app.controllers').controller('MainController', function ($scope, $document) {
 
     $scope.toTheTop = function() {
         $document.scrollTopAnimated(0).then(function() {
---
+++ public/scripts/controllers/search-controller.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** Controllers */
+angular.module('app.controllers').controller('SearchController', function ($scope, WordsService) {
+    $scope.selected = undefined;
+    $scope.words = [];
+//    var isAutocompelteEnabled = true;
+    $scope.onedit = function () {
+//        if (isAutocompelteEnabled) {
+        $scope.words = [];
+        WordsService.searchWords($scope.selected, 'ru', function (searchingData) {
+//                isAutocompelteEnabled = false;
+            $scope.words = searchingData;
+//            duplicates apears here
+            /*                console.log(searchingData + ' : ' + searchingData);
+             if (angular.isArray(searchingData)) {
+             $scope.words = [];
+             searchingData.reduce(function (sequence, word) {
+             return sequence.then(function () {
+             return WordsService.getMeaningSync(word, 'ru')
+             }).then(function (meaning) {
+             if (meaning.length > 0) {
+             $scope.words.push(word);
+             } else {
+             console.log(word + "-" + meaning);
+             }
+             });
+             }, Promise.resolve()).then(function () {
+             isAutocompelteEnabled = true;
+             });
+             }*/
+        });
+//        }
+    }
+
+});
